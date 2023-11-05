8662ec208d57555d5e5f8013b3dba785a626a32b
sakib071 <c193055@ugrad.iiuc.ac.bd>
2023-11-05 20:34:44 +0600

    Project setup done

---
+++ src/components/Firebase/firebase.config.js
@@ -0,0 +1,18 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+    apiKey: "AIzaSyCdta_Fx5YPUtinHTP6k8wWOrlLKewvqAo",
+    authDomain: "job-finder-app-83469.firebaseapp.com",
+    projectId: "job-finder-app-83469",
+    storageBucket: "job-finder-app-83469.appspot.com",
+    messagingSenderId: "965131145679",
+    appId: "1:965131145679:web:d9aa3db48e3416b838c2ba"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+export default app;
